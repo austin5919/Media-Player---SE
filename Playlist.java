386e5afb9c588edb25983b7e8c523709ea526b27
austin5919 <austin5919@gmail.com>
2017-09-18 13:54:44 -0400

    Playlist First Commit

---
+++ Playlist.java
@@ -0,0 +1,34 @@
+
+public class Playlist {
+	
+	//fields
+	
+	
+	/**
+     * Constructor for class Playlist
+     */
+    public Playlist() {
+        
+    }
+	
+	/**
+     * Returns a string version of this.
+     *
+     * @return  A string representation of this Playlist.
+     */
+    public String toString() {
+        return "Coming soon...";
+    }
+
+	/**
+	 * Unit test.
+	 *
+	 *@param args  Command-line parameters for this test. Currently unused
+	 */
+	public static void main(String[] args) {
+		
+	}
+
+	
+	
+} //end of Playlist.java
