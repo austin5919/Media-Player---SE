d0ab3c1ba263120e91194f8d267f2296d08c50c6
joseavel <31928304+joseavel@users.noreply.github.com>
2017-09-27 18:57:12 -0400

    Add files via upload

---
+++ Play.java
@@ -0,0 +1,30 @@
+package bestmusicplayer;
+
+
+public class Play implements SongState{
+    
+    MusicPlayer player;
+    
+    public Play(MusicPlayer player){
+        this.player = player;
+    }
+
+    @Override
+    public void playSong() {
+        
+        this.player.player.play();
+        this.player.setState(this.player.getLaodingSongState());
+        
+    }
+
+    @Override
+    public void stopSong() {
+        //do nothing
+    }
+
+    @Override
+    public void loadSong(String song) {
+        //do nothing
+    }
+    
+}
---
+++ SongState.java
@@ -0,0 +1,8 @@
+package bestmusicplayer;
+
+public interface SongState {
+    public void playSong();
+    public void stopSong();
+    public void loadSong(String song);
+    
+}
